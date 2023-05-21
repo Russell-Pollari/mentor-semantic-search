c55047f2c7b52d400d0115672ffaa19b80617c59
russell-pollari <pollarir@mgail.com>
2023-05-21 07:31:28 -0400

    Add argparse and cleanup some functions

--- create_embeddings.py
+++ create_embeddings.py
@@ -71,43 +71,52 @@ def get_mentor_sentences(path_to_file):
     return mentor_sentences
 
 
-def create_mentor_embeddings(mentor_sentences):
+def get_chroma_client():
     chroma_client = chromadb.Client(chromadb.config.Settings(
         chroma_db_impl="duckdb+parquet",
         persist_directory=PERSIST_DIRECTORY,
     ))
 
-    collection = chroma_client.get_or_create_collection(
-        name="mentors",
-        embedding_function=EMBD_FNC
+    return chroma_client
+
+
+def create_mentor_embeddings(mentor_sentences, collection_name="mentors"):
+    chroma_client = get_chroma_client()
+
+    collection = chroma_client.create_collection(
+        name=collection_name,
+        embedding_function=EMBD_FNC,
+        # metadata={
+        #     "hnsw:space": "cosine",
+        #     "hnsw:construction_ef": "20",
+        #     "hnsw:M": "20",
+        # } 
     )
 
-    collection.upsert(
+    collection.add(
         ids=[mentor['id'] for mentor in mentor_sentences],
         documents=[mentor['sentence'] for mentor in mentor_sentences],
-        metadatas=[mentor['metadata'] for mentor in mentor_sentences]
+        metadatas=[mentor['metadata'] for mentor in mentor_sentences],
     )
 
     return collection
 
 
-def get_mentor_embeddings():
-    chroma_client = chromadb.Client(chromadb.config.Settings(
-        chroma_db_impl="duckdb+parquet",
-        persist_directory=PERSIST_DIRECTORY,
-    ))
-    collection = chroma_client.get_collection(name="mentors", embedding_function=EMBD_FNC)
-
+def get_mentor_embeddings(collection_name="mentors"):
+    chroma_client = get_chroma_client()
+    collection = chroma_client.get_collection(name=collection_name, embedding_function=EMBD_FNC)
     return collection
 
 
 if __name__ == '__main__':
-    try:
-        path_to_file = sys.argv[1]
-    except:
-        path_to_file = 'data/mentors.json'
+    parser = argparse.ArgumentParser(description='Create mentor embeddings')
+    parser.add_argument('--path-to-data', help='Path to the data directory', default='data/mentors.json')
+    parser.add_argument('--collection-name', help='Name of the collection', default='mentors')
+
+    args = parser.parse_args()
     
-    mentor_sentences = get_mentor_sentences('data/mentors.json')
-    print ('creating mentor embeddings')
-    collection = create_mentor_embeddings(mentor_sentences)
-    print('done')
+    mentor_sentences = get_mentor_sentences(args.path_to_data)
+
+    print ('Creating collection {} for {} sentences'.format(args.collection_name, len(mentor_sentences)))
+
+    collection = create_mentor_embeddings(mentor_sentences, collection_name=args.collection_name)
